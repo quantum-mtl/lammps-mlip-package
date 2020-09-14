3ab2863839d7819d4d8f9a7e109e5340947146ac
lan496 <kohei19950508@gmail.com>
2020-09-14 09:55:55 +0000

    Add docker environment and example file

---
+++ docker/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -eu
+
+lammps_mlip_root="$(dirname $(realpath $0))/.."
+lammps_root="${lammps_mlip_root}/lammps"
+ncores="32"
+
+# copy lammps-mlip
+cp -r "${lammps_mlip_root}/lib/mlip" "${lammps_root}/lib"
+cp -r "${lammps_mlip_root}/src/USER-MLIP" "${lammps_root}/src"
+
+# modify Makefile
+cp "${lammps_mlip_root}/docker/Makefile.lammps" "${lammps_root}/lib/mlip/Makefile.lammps"
+cp "${lammps_mlip_root}/docker/Makefile" "${lammps_root}/src/Makefile"
+
+# make
+curr="$(pwd)"
+cd "${lammps_root}/src"
+make yes-user-mlip
+make ps
+make serial -j "${ncores}"
+cp lmp_serial "${lammps_mlip_root}"
+cd "${curr}"
