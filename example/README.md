3a9634c6573362fb7ec222611ef8128affe429d2
lan496 <kohei19950508@gmail.com>
2020-09-14 10:29:02 +0000

    Add more time-consuming benchmark

---
+++ example/README.md
@@ -0,0 +1,5 @@
+```
+cd example
+./../lmp_serial -in in.mlip
+```
+It takes about 40 seconds for 512 atoms with FCC-perturbed TiAl3 structure in a single thread.
---
+++ example/make_lammps_structure.py
@@ -0,0 +1,22 @@
+import numpy as np
+from pymatgen.core import Structure
+from pymatgen.io.lammps.data import LammpsData
+
+
+if __name__ == '__main__':
+    # TiAl3
+    matrix = 4.0 * np.eye(3)
+    species = ['Ti', 'Al', 'Al', 'Al']
+    frac_coords = [
+        [0, 0, 0],
+        [0, 0.5, 0.5],
+        [0.5, 0, 0.5],
+        [0.5, 0.5, 0],
+    ]
+    structure = Structure(matrix, species, frac_coords)
+    structure.make_supercell([2, 2, 2])
+    structure.perturb(0.1)
+
+    ld = LammpsData.from_structure(structure)
+    # output file need to be fixed
+    ld.write_file("structure.lammps")
