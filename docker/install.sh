eba8faa40696d2e5aec15a96bfa5dc63ba935aa2
lan496 <kohei19950508@gmail.com>
2021-02-28 12:49:56 +0900

    refactor

--- docker/install.sh
+++ docker/install.sh
@@ -2,7 +2,6 @@
 
 lammps_mlip_root="$(dirname $(realpath $0))/.."
 lammps_root="${lammps_mlip_root}/lammps"
-ncores="32"
 
 # copy lammps-mlip
 cp -r "${lammps_mlip_root}/lib/mlip" "${lammps_root}/lib"
@@ -18,6 +17,6 @@ cd "${lammps_root}/src"
 make yes-manybody
 make yes-user-mlip
 make ps
-make serial -j "${ncores}"
+make serial -j
 cp lmp_serial "${lammps_mlip_root}"
 cd "${curr}"
