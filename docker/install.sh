7155e07d64f5a11f28c1c75911f94fcd7a0550fb
lan496 <kohei19950508@gmail.com>
2021-03-04 14:39:53 +0000

    Build LAMMPS with Kokkos and OpenMP

--- docker/install.sh
+++ docker/install.sh
@@ -1,5 +1,15 @@
 #!/bin/sh -eu
 
+if [ $# -gt 1 ]; then
+    echo "[Usage] install.sh [ncores]"
+    exit 1
+elif [ $# -eq 1 ]; then
+    ncores=$1
+else
+    ncores=1
+fi
+
+
 lammps_mlip_root="$(dirname $(realpath $0))/.."
 lammps_root="${lammps_mlip_root}/lammps"
 
@@ -10,13 +20,15 @@ cp -r "${lammps_mlip_root}/src/USER-MLIP" "${lammps_root}/src"
 # modify Makefile
 cp "${lammps_mlip_root}/docker/Makefile.lammps" "${lammps_root}/lib/mlip/Makefile.lammps"
 cp "${lammps_mlip_root}/docker/Makefile" "${lammps_root}/src/Makefile"
+cp "${lammps_mlip_root}/docker/Makefile.mlip_kokkos" "${lammps_root}/src/MAKE/Makefile.mlip_kokkos"
 
 # make
 curr="$(pwd)"
 cd "${lammps_root}/src"
+make yes-kokkos
 make yes-manybody
 make yes-user-mlip
 make ps
-make serial -j
-cp lmp_serial "${lammps_mlip_root}"
+make mlip_kokkos -j ${ncores}
+cp lmp_mlip_kokkos "${lammps_mlip_root}"
 cd "${curr}"
